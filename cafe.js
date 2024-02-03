297a298632b2f3a0c88a55f20d1cad1bc90fe6d9
arrizkyocta32 <144558025+arrizkyocta32@users.noreply.github.com>
2024-02-03 22:47:23 +0700

    Add files via upload

---
+++ cafe.js
@@ -0,0 +1,10 @@
+var coffee = prompt("Do you like coffee?");
+if(coffee)
+{
+    console.log('Yes i like drink coffee in morning!');
+}
+else
+{  
+    console.log('I like drinking coffee in the morning');
+
+} 
